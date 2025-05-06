e1bcd5364ca31b1f22e1d5d63eb29385968bcfbe
Trishaan <tshetty1@my.bcit.ca>
2025-05-05 17:43:02 -0700

    corrected membersOnly.js

--- scripts/membersOnly.js
+++ scripts/membersOnly.js
@@ -5,12 +5,11 @@ function logout() {
   window.onload = function () {
     const imagePlaceholder = document.querySelector(".image-placeholder");
 
-    const images = ["/public/dog11.jpg", "/public/dog2.jpg", "/public/dog3.jpg"];
+    const images = ["/public/dog1.jpg", "/public/dog2.jpg", "/public/dog3.jpeg"];
     const randomImage = images[Math.floor(Math.random() * images.length)];
 
     const imgElement = document.createElement("img");
     imgElement.src = randomImage;
 
-    imagePlaceholder.innerHTML = '';
     imagePlaceholder.appendChild(imgElement);
 };
