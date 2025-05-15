1b3ff96e567ad7f154ec5824e23a82d06a06b803
Trishaancodes <tshetty1@my.bcit.ca>
2025-05-15 10:23:33 -0700

    Update database.js

--- scripts/database.js
+++ scripts/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const MONGODB_URI=process.env.MONGODB_URI
-const client = new MongoClient(atlasURI);
+const client = new MongoClient(MONGODB_URI);
 
 let database;
 
