1489b3eb71f400fab720a4699dfc2199ffe3a3af
Trishaancodes <tshetty1@my.bcit.ca>
2025-05-15 10:20:52 -0700

    Update database.js

--- scripts/database.js
+++ scripts/database.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-const mongodb_host = process.env.MONGODB_HOST;
-const mongodb_user = process.env.MONGODB_USER;
-const mongodb_password = process.env.MONGODB_PASSWORD;
-
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority`;
+const MONGODB_URI=process.env.MONGODB_URI
 const client = new MongoClient(atlasURI);
 
 let database;
